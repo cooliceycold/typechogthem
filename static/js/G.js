8126208bc26706f17e8b017976168c30c72b288b
cooliceycold <112804160+cooliceycold@users.noreply.github.com>
2024-07-14 00:02:07 +0800

    Update G.js

--- static/js/G.js
+++ static/js/G.js
@@ -463,7 +463,7 @@ let doLazyload = () => {
 		"img:not(#header-background):not(#profile-avatar)"
 	);
 	preLazy(banners, "style", "origin");
-	preLazy(pics, "src", "origin", window.G_CONFIG.imgUrl + "loading2.gif");
+	preLazy(pics, "src", "origin", window.G_CONFIG.imgUrl + "loading2.webp");
 	lazyload(banners, function (element, observe) {
 		lazyBanner(element, observe);
 	});
